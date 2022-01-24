07d41f1d00c20a0ce9e3202b577d8422f6e09a20
Abhay Singh <abhay5349singh@gmail.com>
2022-01-24 20:27:22 +0530

    Add files via upload

---
+++ Google/Arrays and Strings/20. Valid Parentheses.cpp
@@ -0,0 +1,26 @@
+Question Link: https://leetcode.com/problems/minimum-window-substring/
+
+class Solution {
+public:
+    
+    bool check(char ch, char top){
+        return ((top=='(' && ch==')') || (top=='[' && ch==']') || (top=='{' && ch=='}'));
+    }
+    
+    bool isValid(string s) {
+        int n=s.length();
+        
+        stack<char> st;
+        for(int i=0;i<n;i++){
+            char ch=s[i];
+            
+            if(ch=='(' || ch=='[' || ch=='{'){
+                st.push(ch);
+            }else{
+                if(st.size()==0 || check(ch,st.top())==false) return false;
+                st.pop();
+            }
+        }
+        return (st.size()==0);
+    }
+}; 
