b50d10a6e0d6ade7ab3f19b22bdc9ca4af2aa0cd
Abhay Singh <abhay5349singh@gmail.com>
2022-01-25 20:09:00 +0530

    Update 857. Minimum Cost to Hire K Workers.cpp

--- Google/Arrays and Strings/857. Minimum Cost to Hire K Workers.cpp
+++ Google/Arrays and Strings/857. Minimum Cost to Hire K Workers.cpp
@@ -1,4 +1,6 @@
-Question Link: https://leetcode.com/problems/trapping-rain-water/
+Question Link: https://leetcode.com/problems/minimum-cost-to-hire-k-workers/
+
+// Approach 1:
 
 class Solution {
 public:
@@ -41,6 +43,47 @@ public:
                 pq.pop();
             }
             if(ans > cost) ans=cost;
+        }
+        return ans;
+    }
+};
+
+// Approach 2:
+
+class Solution {
+public:
+    
+    double mincostToHireWorkers(vector<int>& quality, vector<int>& wage, int k) {
+        vector<pair<double,int>> v;
+        int n=quality.size();
+        for(int i=0;i<n;i++){
+            double factor=wage[i]/(double)quality[i];
+            v.push_back({factor,quality[i]});
+        }
+        
+        sort(v.begin(),v.end());
+        
+        priority_queue<int> pq; // max heap
+        
+        double ans=DBL_MAX;
+        int q=0;
+        for(int i=0;i<n;i++){
+            if(pq.size()<k){
+                pq.push(v[i].second);
+                q += v[i].second;
+            }else if(pq.size()==k){
+                if(pq.top()>v[i].second){
+                    q -= pq.top();
+                    pq.pop();
+                    
+                    pq.push(v[i].second);
+                    q += v[i].second;
+                }
+            }
+            if(pq.size()==k){
+                double cost = q*v[i].first;
+                if(ans > cost) ans=cost;
+            }
         }
         return ans;
     }
