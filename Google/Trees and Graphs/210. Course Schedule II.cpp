8db28f1a0580ac42d1c4f58e5ccd272f78b2c364
Abhay Singh <abhay5349singh@gmail.com>
2022-01-26 00:48:29 +0530

    Add files via upload

---
+++ Google/Trees and Graphs/210. Course Schedule II.cpp
@@ -0,0 +1,38 @@
+Question Link: https://leetcode.com/problems/trapping-rain-water/
+
+class Solution {
+public:
+    
+    vector<int> findOrder(int n, vector<vector<int>>& pre) {
+        vector<int> graph[n], indegree(n,0);
+        
+        for(int i=0;i<pre.size();i++){
+            int u=pre[i][0], v=pre[i][1];
+            graph[v].push_back(u);
+            indegree[u]++;
+        }
+        
+        queue<int> q;
+        for(int i=0;i<n;i++){
+            if(indegree[i]==0){
+                q.push(i);
+            }
+        }
+        int c=0;
+        vector<int> ans(n);
+        while(q.size()>0){
+            int src=q.front();
+            q.pop();
+            ans[c]=src;
+            c++;
+            
+            for(int i=0;i<graph[src].size();i++){
+                int child=graph[src][i];
+                indegree[child]--;
+                if(indegree[child]==0) q.push(child);
+            }
+        }
+        if(c<n) return {};
+        return ans; 
+    }
+};
