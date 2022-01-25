ee62fccf3a3c089414d70880372dbd0bf2ba1c06
Abhay Singh <abhay5349singh@gmail.com>
2022-01-25 20:35:51 +0530

    Add files via upload

---
+++ Google/Linked Lists/19. Remove Nth Node From End of List.cpp
@@ -0,0 +1,24 @@
+Question Link: https://leetcode.com/problems/trapping-rain-water/
+
+class Solution {
+public:
+    ListNode* removeNthFromEnd(ListNode* head, int k) {
+        ListNode *p, *q, *pre;
+        pre=NULL;
+        p=q=head;
+        
+        for(int i=0;i<k;i++) q=q->next;
+        
+        while(q!=NULL){
+            pre=p;
+            p=p->next;
+            q=q->next;
+        }
+        
+        if(pre==NULL) return head->next;
+    
+        pre->next=p->next;
+        
+        return head;
+    }
+};
