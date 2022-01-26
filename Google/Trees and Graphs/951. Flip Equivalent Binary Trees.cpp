1ff0fe37a3ba7f44ce4b9f03186eaf79e7ed3d36
Abhay Singh <abhay5349singh@gmail.com>
2022-01-27 03:31:29 +0530

    Add files via upload

---
+++ Google/Trees and Graphs/951. Flip Equivalent Binary Trees.cpp
@@ -0,0 +1,19 @@
+Question Link: https://leetcode.com/problems/trapping-rain-water/
+
+class Solution {
+public:
+    bool flipEquiv(TreeNode* p, TreeNode* q) {
+        if(p==NULL && q==NULL) return true;
+        if(p==NULL || q==NULL) return false;
+        
+        if(p->val != q->val) return false;
+        
+        if(flipEquiv(p->left,q->left)==true && flipEquiv(p->right,q->right)==true){
+            return true;
+        }else if(flipEquiv(p->left,q->right)==true && flipEquiv(p->right,q->left)==true){
+            return true;
+        }
+        
+        return false;
+    }
+};
