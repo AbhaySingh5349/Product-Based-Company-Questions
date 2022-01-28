6990f8252a6737c451593692b5f16f6add8d81d2
Abhay Singh <abhay5349singh@gmail.com>
2022-01-29 02:00:19 +0530

    Update 53. Maximum Subarray.cpp

--- Google/Dynamic Programming/53. Maximum Subarray.cpp
+++ Google/Dynamic Programming/53. Maximum Subarray.cpp
@@ -1,11 +1,13 @@
 Question Link: https://leetcode.com/problems/maximum-subarray/
 
+// Approach 1:
+
 class Solution {
 public:
     int maxSubArray(vector<int>& a) {
         int n=a.size();
         
-     /*   int dp[n];
+        int dp[n];
         dp[0]=a[0];
         
         int ans=a[0];
@@ -13,7 +15,16 @@ public:
             dp[i]=max(dp[i-1]+a[i],a[i]);
             ans=max(ans,dp[i]);
         }
-        return ans; */
+        return ans; 
+    }
+};
+
+// Approach 2:
+
+class Solution {
+public:
+    int maxSubArray(vector<int>& a) {
+        int n=a.size();
         
         int ans=a[0];
         int sum=0;
