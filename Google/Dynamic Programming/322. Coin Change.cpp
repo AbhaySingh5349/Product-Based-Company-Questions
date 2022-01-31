187cdb91bcc9ec32d93edaa61455a8c264b0d6a0
Abhay Singh <abhay5349singh@gmail.com>
2022-01-31 10:50:33 +0530

    Update 322. Coin Change.cpp

--- Google/Dynamic Programming/322. Coin Change.cpp
+++ Google/Dynamic Programming/322. Coin Change.cpp
@@ -1,5 +1,7 @@
 Question Link: https://leetcode.com/problems/coin-change/
 
+// Approach 1:
+
 class Solution {
 public:
     
@@ -27,5 +29,27 @@ public:
         int ans=solve(a,k,0);
         
         return (ans>=INT_MAX/2 ? -1:ans);
+    }
+};
+
+// Approach 2:
+
+class Solution {
+public:
+    int coinChange(vector<int> &a, int k) {
+        int n=a.size();
+        
+        vector<vector<int>> dp(n+1,vector<int>(k+1,INT_MAX/2));
+        for(int j=0;j<k+1;j++) dp[0][j]=INT_MAX/2;
+        for(int i=0;i<n+1;i++) dp[i][0]=0;
+        
+        for(int i=1;i<n+1;i++){
+            for(int j=1;j<k+1;j++){
+                dp[i][j]=dp[i-1][j];
+                if(j>=a[i-1] && dp[i][j-a[i-1]]!=INT_MAX/2) dp[i][j] = min(dp[i][j],1+dp[i][j-a[i-1]]);
+            }
+        }
+        
+        return (dp[n][k]>=INT_MAX/2 ? -1:dp[n][k]);
     }
 };
