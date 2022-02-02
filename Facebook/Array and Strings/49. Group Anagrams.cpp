1a74773af5ebe1f6bfccc75dd2351c3a01402f24
Abhay Singh <abhay5349singh@gmail.com>
2022-02-03 00:18:54 +0530

    Add files via upload

---
+++ Facebook/Array and Strings/49. Group Anagrams.cpp
@@ -0,0 +1,39 @@
+Question Link: https://leetcode.com/problems/trapping-rain-water/
+
+class Solution {
+public:
+    vector<vector<string>> groupAnagrams(vector<string>& v) {
+        int m=v.size();
+        
+        map<map<char,int>,vector<string>> bm;
+        
+        for(int i=0;i<m;i++){
+            map<char,int> sm;
+            
+            string s=v[i];
+            int n=s.length();
+            
+            for(int i=0;i<n;i++){
+                char ch=s[i];
+                sm[ch]++;
+            }
+            
+            vector<string> cur;
+            if(bm.find(sm)!=bm.end()) cur=bm[sm];
+            cur.push_back(s);
+            
+            bm[sm]=cur;
+        }
+        
+        vector<vector<string>> ans;
+            
+        map<map<char,int>,vector<string>> :: iterator it;
+        for(it=bm.begin();it!=bm.end();it++){
+            vector<string> cur=it->second;
+            
+            ans.push_back(cur);
+        }
+        
+        return ans;
+    }
+};
