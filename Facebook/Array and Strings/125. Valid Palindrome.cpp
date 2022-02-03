9f8bc08768efeed3b3d9c1a5cdd0d8cf22d60054
Abhay Singh <abhay5349singh@gmail.com>
2022-02-03 14:39:22 +0530

    Add files via upload

---
+++ Facebook/Array and Strings/125. Valid Palindrome.cpp
@@ -0,0 +1,30 @@
+Question Link: https://leetcode.com/problems/trapping-rain-water/
+
+class Solution {
+public:
+    
+    char lower(char ch){
+        if(ch>='A' && ch<='Z') ch=ch-'A'+'a';
+        return ch;
+    }
+    
+    bool isPalindrome(string s) {
+        int n=s.length();
+        int i=0, j=n-1;
+        
+        while(i<j){
+            
+            while(i<j && !isdigit(s[i]) && !isalpha(s[i])) i++;
+            while(i<j && !isdigit(s[j]) && !isalpha(s[j])) j--;
+            
+            if(i>=j) return true;
+            
+            char ch1=lower(s[i]);
+            char ch2=lower(s[j]);
+            
+            if(ch1!=ch2) return false;
+            i++, j--;
+        }
+        return true;
+    }
+};
