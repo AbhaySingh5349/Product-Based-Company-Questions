fe2bbc14eb3462999f40f6322a84efdfc3f503c0
Abhay Singh <abhay5349singh@gmail.com>
2022-02-03 14:32:23 +0530

    Update 88. Merge Sorted Array.cpp

--- Facebook/Array and Strings/88. Merge Sorted Array.cpp
+++ Facebook/Array and Strings/88. Merge Sorted Array.cpp
@@ -1,5 +1,7 @@
 Question Link: https://leetcode.com/problems/merge-sorted-array/
 
+// Approach 1:
+
 class Solution {
 public:
     
@@ -19,7 +21,7 @@ public:
     }
     
     void merge(vector<int> &a, int m, vector<int> &b, int n) {
-        rotate(a,n);
+        rotate(a,n); // shifting 'a' elements to right
         
         int size=m+n;
         
@@ -33,6 +35,27 @@ public:
             }else{
                 a[k++]=y;
                 j += (j<n);
+            }
+        }
+    }
+};
+
+// Approach 2:
+
+class Solution {
+public:
+    
+    void merge(vector<int> &a, int m, vector<int> &b, int n) {
+        int i=m-1, j=n-1, k=m+n-1;
+        while(i>=0 || j>=0){
+            int x = (i>=0 ? a[i]:INT_MIN), y = (j>=0 ? b[j]:INT_MIN);
+            
+            if(x>=y){
+                a[k--]=x;
+                i -= (i>=0);
+            }else{
+                a[k--]=y;
+                j -= (j>=0);
             }
         }
     }
