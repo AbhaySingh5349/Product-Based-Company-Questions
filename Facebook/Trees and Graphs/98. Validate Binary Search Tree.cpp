850500413593bec0879391a2339bd41812c42e5c
Abhay Singh <abhay5349singh@gmail.com>
2022-02-04 21:23:39 +0530

    Update 98. Validate Binary Search Tree.cpp

--- Facebook/Trees and Graphs/98. Validate Binary Search Tree.cpp
+++ Facebook/Trees and Graphs/98. Validate Binary Search Tree.cpp
@@ -93,3 +93,31 @@ public:
         return check(root).isBST;
     }
 };
+
+// Approach 3:
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    
+    bool isValid(TreeNode* root, TreeNode* minNode, TreeNode* maxNode) {
+        if(!root) return true;
+        if(minNode && root->val <= minNode->val || maxNode && root->val >= maxNode->val)
+            return false;
+        return isValid(root->left, minNode, root) && isValid(root->right, root, maxNode);
+    }
+    
+    bool isValidBST(TreeNode* root) {
+        return isValid(root,NULL,NULL);
+    }
+};
