f293b7ec58f8ae865c7f42758cad9c6e252d9285
Abhay Singh <abhay5349singh@gmail.com>
2022-02-04 10:56:18 +0530

    Add files via upload

---
+++ Facebook/Trees and Graphs/98. Validate Binary Search Tree.cpp
@@ -0,0 +1,44 @@
+Question Link: https://leetcode.com/problems/validate-binary-search-tree/
+
+class Solution {
+public:
+    
+    #define ll long long 
+    
+    struct Info{
+        bool isBST;
+        ll int submax;
+        ll int submin;
+    };
+    
+    ll getMax(ll int a, ll int b){
+        if(a>=b) return a;
+        return b;
+    }
+    
+    ll getMin(ll int a, ll int b){
+        if(a>b) return b;
+        return a;
+    }
+    
+    Info check(TreeNode* root){
+        if(root==NULL) return {true,LONG_MIN,LONG_MAX};
+        
+        Info leftInfo=check(root->left);
+        Info rightInfo=check(root->right);
+        
+        int val=root->val;
+        bool curBST=(val>leftInfo.submax && val<rightInfo.submin && leftInfo.isBST && rightInfo.isBST);
+        
+        ll int curmax=getMax(val,max(leftInfo.submax,rightInfo.submax));
+        ll int curmin=getMin(val,min(leftInfo.submin,rightInfo.submin));
+        
+        Info cur={curBST,curmax,curmin};
+        return cur;
+    }
+    
+    bool isValidBST(TreeNode* root) {
+        if((root==NULL) || (root->left==NULL && root->right==NULL)) return true;
+        return check(root).isBST;
+    }
+};
